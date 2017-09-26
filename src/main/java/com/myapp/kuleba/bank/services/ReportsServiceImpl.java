2fc7989823e10840ff6e0c768078ff3b405567cf
Oleg Kuleba <oleg.kuleba@gmail.com>
2017-09-26 23:11:19 +0300

    TODO was added in getGeneralReport() method

--- src/main/java/com/myapp/kuleba/bank/services/ReportsServiceImpl.java
+++ src/main/java/com/myapp/kuleba/bank/services/ReportsServiceImpl.java
@@ -73,7 +73,7 @@ public class ReportsServiceImpl implements ReportsService {
 
     @Override
     public List getGeneralReport() {
-
+        //TODO Will be written in future
         return null;
     }
 }
